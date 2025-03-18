248af52c7b4da8d533e3c5d7be65b597accea284
Carl-Robert <robertm6ts@gmail.com>
2025-03-18 13:23:22 +0200

    User login and logout with localStorage

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import Card from '../UI/Card'
+import './Home.css'
+
+const Home = () =>{
+    return (
+        <Card className="home">
+            <h1>Welcome back, u shall not leave!</h1>
+        </Card>
+    )
+} 
+
+export default Home
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,26 @@
 import './Login.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 
-const Login = () => {
+const Login = (props) => {
     const [enteredEmail, setEnteredEmail] = useState('')
     const [enteredPassword, setEnteredPassword] = useState('')
     const [emailIsValid, setEmailIsValid] = useState()
     const [passwordIsValid, setPasswordIsValid] = useState()
+    const [formIsValid, setFormIsValid] = useState(false) 
     
+    useEffect(() => {
+        const timeOut = setTimeout(() =>{
+            console.log('check form is valid')
+            setFormIsValid(emailIsValid && passwordIsValid)
+            console.log('checked')
+        }, 500 )
+        return () =>{
+            clearTimeout(timeOut)
+        }  
+    },[emailIsValid, passwordIsValid])
+
     const emailChangeHandler = (event) => {
         setEnteredEmail(event.target.value);
     };
@@ -25,9 +37,13 @@ const Login = () => {
     const passwordValidateHandler = () => {
         setPasswordIsValid(enteredPassword.trim().length > 6);
     };
+    const submitHandler = (event) => {
+        event.preventDefault()
+        props.onLogin(enteredEmail, enteredPassword)
+    } 
     return (
         <Card className="login">
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className={`control ${emailIsValid === false ? 'invalid':''}`}>
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email"value={enteredEmail} onChange={emailChangeHandler} onBlur={emailValidateHandler}></input>
@@ -37,7 +53,7 @@ const Login = () => {
                     <input type="password" id="password" value={enteredPassword} onChange={passwordChangeHandler} onBlur={passwordValidateHandler}></input>
                 </div>
                 <div className="actions">
-                    <Button type="submit">
+                    <Button type="submit" disabled={!formIsValid} >
                         Login
                     </Button>
                 </div>
